bf2f3ece4be661427fe211ceb6a6a8f800473da4
Pasha Chernyshev <pavel.che.che.che@gmail.com>
2026-06-09 18:28:42 +0400

    Revert "chore: add migration SQL for keyword_scan_result column"
    This reverts commit ce5b141c66d409a3205ca501e46342f9372ff175.

--- supabase_migration_keyword_scan_result.sql
+++
@@ -1,4 +0,0 @@
--- Migration: add keyword_scan_result column to projects table
--- Run this in Supabase dashboard → SQL Editor
-
-ALTER TABLE projects ADD COLUMN IF NOT EXISTS keyword_scan_result bytea;
